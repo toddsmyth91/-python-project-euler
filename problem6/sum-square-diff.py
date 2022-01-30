07eda7337c1539799ec4abcd776ab2facba05dba
Todd Smyth <toddsmyth91@gmail.com>
2022-01-30 14:22:12 +1000

    problem 6 complete - not a very difficult one liner...

--- problem6/sum-square-diff.py
+++ problem6/sum-square-diff.py
@@ -7,3 +7,15 @@ Hence the difference between the sum of the squares of the first ten natural num
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 '''
+
+# sum of squares of first 10 natural numbers
+# print(sum([x**2 for x in range(1,11)]))
+
+# square of sum of first 10 natural numbers
+# print(sum(x for x in range(1,11))**2)
+
+# difference between sum of squares and square of sum	
+print(sum(x for x in range(1,11))**2 - sum([x**2 for x in range(1,11)]))
+
+# difference between sum of squares and square of sum of first 100 numbers
+print(sum(x for x in range(1,101))**2 - sum([x**2 for x in range(1,101)]))
